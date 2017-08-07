fbfd033306e6142faed7b227023134b81027cc9b
vijayashangavi <vijayashangavi6@gmail.com>
2017-08-07 12:59:08 +0530

    erased old

--- includes/masterFoot.php
+++
@@ -1,6 +0,0 @@
-</div>
-<footer class="container-fluid text-center footer-bg">
-  <p>@Smart Health Care System by <a href="https://www.SmartHealthCareSystem.com">www.SmartHealthCareSystem.com</a></p> 
-</footer>
-</body>
-</html>
